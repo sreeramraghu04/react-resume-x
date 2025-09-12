296ec3830b200e82bd7256ba25c712246550c3a8
sreeramraghu04 <sreeramraghu04@gmail.com>
2025-09-12 20:03:46 +0530

    final

--- src/components/steps/steps.js
+++ src/components/steps/steps.js
@@ -2,7 +2,7 @@ export const steps = [
   { id: 1, title: "Personal" },
   { id: 2, title: "Education" },
   { id: 3, title: "Experience" },
-  { id: 4, title: "Skills" },
-  { id: 5, title: "Projects" },
+  { id: 4, title: "Skill" },
+  { id: 5, title: "Project" },
   { id: 6, title: "Preview" },
 ];
